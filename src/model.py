7f69d2660d8783e040aef601b4e4e90172ffd916
ben <cohen.benr@gmail.com>
2020-06-19 10:57:48 +0000

    moved computations to gpu

--- src/model.py
+++ src/model.py
@@ -11,12 +11,20 @@ class StarSpace(nn.Module):
         self.n_input = len(vocabulary)
         self.vocab = vocabulary
         self.k_neg = k_neg
+        self.d_embed = d_embed
         
         if input_embedding is None:
-            self.embeddings = nn.Embedding(self.n_input, d_embed, max_norm=max_norm)
+            self.embeddings = nn.Embedding(self.n_input, self.d_embed, max_norm=max_norm)
         else:
             self.embeddings = input_embedding
-    
+        
+        if torch.cuda.is_available():
+            self.device = 'cuda'
+        else:
+            self.device = 'cpu'
+            
+        self.embeddings.to(self.device)
+        
     def get_positions(self,train):
         """ Get the positions of every word.  Return list of list of tensors """
         train_pos = []
@@ -35,7 +43,11 @@ class StarSpace(nn.Module):
                         positions.append(self.vocab[tok])
                     except KeyError:
                         pass
-
+                
+                #TEMPORARY FIX- a totally neutral but not too common word
+                if len(positions) < 1:
+                    positions.append(self.vocab['able'])
+                    
                 doc_positions.append(torch.LongTensor(positions))
 
             train_pos.append(doc_positions)
@@ -52,9 +64,13 @@ class StarSpace(nn.Module):
         return output
     
     def forward(self, docs):       
-        l_batch = []
-        r_batch = []
-        neg_batch = []
+#         l_batch = []
+#         r_batch = []
+#         neg_batch = []
+        
+        l_batch = torch.zeros((len(docs),self.d_embed)).to(self.device)
+        r_batch = torch.zeros((len(docs),self.d_embed)).to(self.device)
+        neg_batch = torch.zeros((len(docs),self.k_neg, self.d_embed)).to(self.device)
         
         for i,s in enumerate(docs):
             #Positive similarity between sentences
@@ -66,18 +82,17 @@ class StarSpace(nn.Module):
                 a = s[choices[0]]
                 b = s[choices[1]]
 
-#             a = a.split()
-#             b = b.split()
-
             a_emb = self.embed_doc(a)
             b_emb = self.embed_doc(b)
 
-            l_batch.append(a_emb)
-            r_batch.append(b_emb)
+            l_batch[i,:] = a_emb
+            r_batch[i,:] = b_emb
 
             #Negative similarity
             negs = []
-            while len(negs) < self.k_neg:
+            negs = torch.zeros((self.k_neg, self.d_embed)).to(self.device)
+            num_negs = 0
+            while num_negs < self.k_neg:
                 index = np.random.choice(len(docs))
                 
                 if index != i: #if it's not from the same document
@@ -86,75 +101,13 @@ class StarSpace(nn.Module):
                     
                     c = neg_doc[neg_choice]
                     c_emb = self.embed_doc(c)
-                    negs.append(c_emb)
+                    
+                    negs[num_negs,:] = c_emb
+                    num_negs += 1
 
-            neg_batch.append(torch.stack(negs))
+            neg_batch[i,:,:] = negs
         
-        l_batch = torch.stack(l_batch)
-        r_batch = torch.stack(r_batch)
-        neg_batch = torch.stack(neg_batch)
-
         l_batch = l_batch.unsqueeze(1)
         r_batch = r_batch.unsqueeze(1)
         
         return l_batch, r_batch, neg_batch
-    
-
-# class MarginRankingLoss(nn.Module):
-#     def __init__(self, margin=1., aggregate=torch.mean):
-#         super(MarginRankingLoss, self).__init__()
-#         self.margin = margin
-#         self.aggregate = aggregate
-
-#     def forward(self, positive_similarity, negative_similarity):
-#         return self.aggregate(
-#             torch.clamp(self.margin - positive_similarity + negative_similarity, min=0))
-    
-
-# class InnerProductSimilarity(nn.Module):
-#     def __init__(self):
-#         super(InnerProductSimilarity, self).__init__()
-
-#     def forward(self, a, b):
-#         # a => B x [n_a x] dim, b => B x [n_b x] dim
-
-#         if a.dim() == 2:
-#             a = a.unsqueeze(1)  # B x n_a x dim
-
-#         if b.dim() == 2:
-#             b = b.unsqueeze(1)  # B x n_b x dim
-
-#         return torch.bmm(a, b.transpose(2, 1))  # B x n_a x n_b
-
-    
-# class StarSpace_old(nn.Module):
-#     def __init__(self, d_embed, n_input, n_output, similarity, max_norm=10, aggregate=torch.sum):
-#         super(StarSpace, self).__init__()
-
-#         self.n_input = n_input
-#         self.n_output = n_output
-#         self.similarity = similarity
-#         self.aggregate = aggregate
-        
-#         self.input_embedding = nn.Embedding(n_input, d_embed, max_norm=max_norm)
-#         self.output_embedding = nn.Embedding(n_output, d_embed, max_norm=max_norm)
-
-#     def forward(self, input=None, output=None):
-#         input_repr, output_repr = None, None
-        
-#         if input is not None:
-            
-#             if input.dim() == 1:
-#                 input = input.unsqueeze(-1)
-
-#             input_emb = self.input_embedding(input)  # B x L_i x dim
-#             input_repr = self.aggregate(input_emb, dim=1)  # B x dim
-        
-#         if output is not None:
-#             if output.dim() == 1:
-#                 output = output.unsqueeze(-1)  # B x L_o
-
-#             output_emb = self.output_embedding(output)  # B x L_o x dim
-#             output_repr = self.aggregate(output_emb, dim=1)  # B x dim
-        
-#         return input_repr, output_repr
